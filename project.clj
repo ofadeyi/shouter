cb863bcc1117d86770cdb42a2eeeebfaba819bf6
ofadeyi <ofadeyi@gmail.com>
2017-03-05 23:22:21 +0000

    LEARN-002 | Ola | Complete working solution

--- project.clj
+++ project.clj
@@ -8,4 +8,14 @@
                  [org.postgresql/postgresql "42.0.0"]
                  [compojure "1.5.2"]
                  [ring/ring-jetty-adapter "1.5.0"]
-                 [hiccup "1.0.5"]])
+                 [hiccup "1.0.5"]
+                 [ring/ring-defaults "0.2.3"]]
+
+  :main ^:skip-aot shouter.web
+  :uberjar-name "shouter-standalone.jar"
+  :plugins [[lein-ring "0.11.0"]]
+  :ring {:handler shouter.web/application
+         :init shouter.models.migration/migrate}
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring-mock "0.1.5"]]}
+             :uberjar {:aot :all}})
--- src/shouter/web.clj
+++ src/shouter/web.clj
@@ -1,19 +1,25 @@
 (ns shouter.web
   (:require [compojure.core :refer [defroutes GET]]
             [ring.adapter.jetty :as ring]
-            [hiccup.page :as page]))
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [shouter.controllers.shouts :as shouts]
+            [shouter.views.layout :as layout]
+            [shouter.models.migration :as schema])
+  (:gen-class))
 
-;; Defining an index function using hiccup
-(defn index []
-  (page/html5
-    [:head
-     [:title "Hello World"]]
-    [:body
-     [:div {:id "content"} "Hello World"]]))
-
-;; Defining a simple compojure route
 (defroutes routes
-           (GET "/" [] (index)))
+           shouts/routes
+           (route/resources "/")
+           (route/not-found (layout/four-oh-four)))
+
+(def application (wrap-defaults routes site-defaults))
+
+(defn start [port]
+  (ring/run-jetty application {:port port
+                               :join? false}))
 
 (defn -main []
-  (ring/run-jetty #'routes {:port 8080 :join? false}))
+  (schema/migrate)
+  (let [port (Integer. (or (System/getenv "PORT") "8080"))]
+    (start port)))
